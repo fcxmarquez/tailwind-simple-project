c70b75dd9bf54de5b0a6d35a9284ee76afa9ac06
fcxmarquez <francisco.marquez.solt@gmail.com>
2023-02-07 08:44:28 -0600

    Tailwind como API para la creacion de un design system

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,13 @@ import reactLogo from "./assets/react.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div className="w-64 h-64 bg-secondary">
+      <h1 className="text-3xl text-primary font-bold underline font-Montserrat">
+        Hello world!
+      </h1>
+    </div>
+  );
 }
 
 export default App;
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,40 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "san-francisco": "url('/src/assets/img/san-francisco.jpg')",
+        "san-francisco-desktop":
+          "url('/src/assets/img/san-franciscoDesktop.jpg')",
+        yosemite: "url('/src/assets/img/yosemite.jpg')",
+        LA: "url('/src/assets/img/LA.jpg')",
+        seattle: "url('/src/assets/img/seattle.jpg')",
+        "new-york": "url('/src/assets/img/new_york.jpg')",
+        norway: "url('/src/assets/img/norway.jpg')",
+        bali: "url('/src/assets/img/bali.jpg')",
+        sydney: "url('/src/assets/img/sydney.jpg')",
+        miami: "url('/src/assets/img/miami.jpg')",
+        switzerland: "url('/src/assets/img/switzerland.jpg')",
+        chicago: "url('/src/assets/img/chicago.jpg')",
+        europe: "url('/src/assets/img/europe.jpg')",
+        iceland: "url('/src/assets/img/iceland.jpg')",
+      },
+      backgroundColor: (theme) => ({
+        ...theme("colors"),
+        primary: "#CC2D4A",
+        secondary: "#8FA206",
+        terciary: "#61AEC9",
+      }),
+      textColor: {
+        primary: "#CC2D4A",
+        secondary: "#8FA206",
+        terciary: "#61AEC9",
+      },
+      fontFamily: {
+        Montserrat: ["Montserrat", "sans-serif"],
+      }
+    },
   },
   plugins: [],
-}
+};
