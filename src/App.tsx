62555ec90ebd87c78a00b465f222637b96e416a3
fcxmarquez <francisco.marquez.solt@gmail.com>
2023-02-17 17:40:33 -0600

    Creando la seccion de Home

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,7 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import "./App.css";
+import { Landing } from "./views/Landing";
 
 function App() {
-  return (
-    <div className="w-64 h-64 bg-secondary">
-      <h1 className="text-3xl text-primary font-bold underline font-Montserrat">
-        Hello world!
-      </h1>
-    </div>
-  );
+  return <Landing />;
 }
 
 export default App;
---
+++ src/views/Landing.tsx
@@ -0,0 +1,34 @@
+import sanFranciscoImg from "../assets/img/sanFrancisco.jpg";
+
+export const Landing = () => {
+  return (
+    <>
+      <nav></nav>
+      <div id="tab-bar"></div>
+      <section className="h-screen w-full">
+        <div className="home">
+          <div className="h-3/4 w-full">
+            <div className="absolute flex h-full w-full flex-col items-center space-y-96 py-4">
+              <input
+                className="rounded-full p-3 shadow-sm"
+                placeholder="San Francisco..."
+                type="search"
+                name=""
+                id=""
+              />
+              <button className="w-36 rounded-full bg-white p-4 text-xl font-semibold text-primary shadow-sm">
+                Explore
+              </button>
+            </div>
+            <div className="h-full w-full">
+              <img src={sanFranciscoImg} />
+            </div>
+          </div>
+        </div>
+        <div id="recommendations"></div>
+        <div id="featured-rents"></div>
+      </section>
+      <footer></footer>
+    </>
+  );
+};
